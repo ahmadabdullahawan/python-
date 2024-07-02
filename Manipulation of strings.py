9bc46faf750b56d231341f9dd2922e71cc23ca82
Abdullah Ahmad <160543040+ahmadabdullahawan@users.noreply.github.com>
2024-07-01 22:50:41 -0700

    Add files via upload

---
+++ Manipulation of strings.py
@@ -0,0 +1,15 @@
+print("HELLO WORLD")
+print('HELLO WORLD')
+a = "PAKISTAN"
+print(a)
+b = ' REMOVE WHITE SPACES '
+print(b.strip())  # remove whitespaces in start and end
+c = "upper case"
+print(c.upper())
+d = "LOWER CASE"
+print(d.lower())
+e = "DAKISTAN"  # REPLACING
+print(e.replace("D","P"))
+m = 'HELLO . WORLD'
+print(m.split(' . '))
+

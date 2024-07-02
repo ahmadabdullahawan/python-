996d30aac7c772bbaa045d9bcc8b3ae3d8f79d9b
Abdullah Ahmad <160543040+ahmadabdullahawan@users.noreply.github.com>
2024-07-01 23:14:28 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+this_list = ["abdullah ", "ali", "khan"]  # list of strings
+print(this_list)
+print(len(this_list))
+list_1 = [1,2,3,4,5]  # list of integers
+print(list_1)
+list_2 = [True,False]  # List of booleans
+print(list_2)
+list_3 = ["ali",1,True] # List of integers booleans and strings
+print(list_3)
